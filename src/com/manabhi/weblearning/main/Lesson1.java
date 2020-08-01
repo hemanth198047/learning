e2b343b566464da82940e12fbb0cdcce17273241
Hemanth Kumar <java.swing.developer@gmail.com>
2020-08-01 21:36:02 +0530

    Change in class structure

--- src/com/manabhi/weblearning/main/Lesson1.java
+++ src/com/manabhi/weblearning/main/Lesson1.java
@@ -2,6 +2,6 @@ package com.manabhi.weblearning.main;
 
 public class Lesson1 {
     public static void main(String... args) {
-        System.out.println("Hello J2Ee World!!");
+        System.out.println("Hello J2EE World!!");
     }
 }
